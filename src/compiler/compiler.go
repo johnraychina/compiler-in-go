919a856cdf3e0d7db4b9b8554cb18893d85a8d7c
johnraychina <johnraychina@163.com>
2022-08-21 20:18:07 +0800

    conditional

--- src/compiler/compiler.go
+++ src/compiler/compiler.go
@@ -7,8 +7,15 @@ import (
 	"monkey/object"
 )
 
+type EmmittedInstruction struct {
+	Opcode   code.Opcode
+	Position int
+}
+
 type Compiler struct {
-	instructions code.Instructions
+	instructions        code.Instructions
+	lastInstruction     EmmittedInstruction
+	previousInstruction EmmittedInstruction
 
 	// 常量池便于减少重复数据消耗内存
 	// instruction只需要引用各操作数的index，格式更加规整，处理逻辑更简单，index固定2个byte（有65535个足够用了）
@@ -17,8 +24,10 @@ type Compiler struct {
 
 func New() *Compiler {
 	return &Compiler{
-		instructions: code.Instructions{},
-		constants:    []object.Object{},
+		instructions:        code.Instructions{},
+		constants:           []object.Object{},
+		lastInstruction:     EmmittedInstruction{},
+		previousInstruction: EmmittedInstruction{},
 	}
 }
 
@@ -109,14 +118,79 @@ func (c *Compiler) Compile(node ast.Node) error {
 		} else {
 			c.emit(code.OpFalse)
 		}
+
+	case *ast.IfExpression:
+		err := c.Compile(node.Condition)
+		if err != nil {
+			return err
+		}
+
+		//emit an 'OpJumpNotTruthy' with a bogus value
+		jumpNotTruthyPos := c.emit(code.OpJumpNotTruthy, 9999)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Consequence)
+		if err != nil {
+			return err
+		}
+
+		if c.lastInstructionIsPop() {
+			c.removeLastPop()
+		}
+
+		afterConsequencePos := len(c.instructions)
+		c.changeOperand(jumpNotTruthyPos, afterConsequencePos)
+
+	case *ast.BlockStatement:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
 	}
 	return nil
+}
+
+func (c *Compiler) replaceInstruction(pos int, newInstructions []byte) {
+	for i := 0; i < len(newInstructions); i++ {
+		c.instructions[i+pos] = newInstructions[i]
+	}
+}
+
+func (c *Compiler) changeOperand(opPos int, operand int) {
+	op := code.Opcode(c.instructions[opPos])
+	newInstruction := code.Make(op, operand)
+
+	c.replaceInstruction(opPos, newInstruction)
+}
+
+func (c *Compiler) lastInstructionIsPop() bool {
+	return c.lastInstruction.Opcode == code.OpPop
+}
+
+func (c *Compiler) removeLastPop() {
+	c.instructions = c.instructions[:c.lastInstruction.Position]
+	c.lastInstruction = c.previousInstruction
 }
 
 func (c *Compiler) emit(op code.Opcode, operands ...int) int {
 	ins := code.Make(op, operands...)
 	pos := c.addInstruction(ins)
+
+	c.setLastInstruction(op, pos)
+
 	return pos
+}
+
+func (c *Compiler) setLastInstruction(op code.Opcode, pos int) {
+	previous := c.lastInstruction
+	last := EmmittedInstruction{Opcode: op, Position: pos}
+
+	c.previousInstruction = previous
+	c.lastInstruction = last
 }
 
 // addConstant add object to compiler.constants and return its index
--- src/compiler/compiler_test.go
+++ src/compiler/compiler_test.go
@@ -16,6 +16,31 @@ type compilerTestCase struct {
 	expectedInstructions []code.Instructions
 }
 
+func TestConditionals(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input:             `if(true){ 10 } 333;`,
+			expectedConstants: []interface{}{10, 333},
+			expectedInstructions: []code.Instructions{
+				//0000
+				code.Make(code.OpTrue),
+				//0001
+				code.Make(code.OpJumpNotTruthy, 7),
+				//0004
+				code.Make(code.OpConstant, 0),
+				//0007
+				code.Make(code.OpPop),
+				//0008
+				code.Make(code.OpConstant, 1),
+				//0011
+				code.Make(code.OpPop),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
+}
+
 func TestBooleanExpression(t *testing.T) {
 	tests := []compilerTestCase{
 		{
